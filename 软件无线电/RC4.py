65a3155492a1ff639cebf76d6a3f7abda2ce89f2
zhang jinyuan <1337200048@qq.com>
2024-12-17 17:23:12 +0800

    modifie

---
+++ 软件无线电/RC4.py
@@ -0,0 +1,130 @@
+import codecs
+import logging
+
+# basic  settings
+logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+MOD = 256
+
+''' @function: generate the dislocated S-box. Key Scheduling Algorithm 
+    @inputs: key->arr;encryption key used for encrypting, as unicode values array
+    @outputs: S->arr;the dislocated S-box
+    @notice: none
+'''
+def KSA(key):
+    key_length = len(key)
+    # create the array "S"
+    S = list(range(MOD))  # [0,1,2, ... , 255]
+    j = 0
+    for i in range(MOD):
+        j = (j + S[i] + key[i % key_length]) % MOD
+        S[i], S[j] = S[j], S[i]  # swap values
+
+    return S
+
+''' @function: generate the generator Obj K to encrypt the text
+    @inputs: S->arr;S-box
+    @outputs: K->generator
+    @notice: none
+'''
+def PRGA(S):
+    i = 0
+    j = 0
+    while True:
+        i = (i + 1) % MOD
+        j = (j + S[i]) % MOD
+
+        S[i], S[j] = S[j], S[i]  # swap values
+        K = S[(S[i] + S[j]) % MOD]
+        yield K
+
+
+
+def encrypt_logic(key, text):
+    ''' :key -> encryption key used for encrypting, as hex string
+        :text -> array of unicode values/ byte string to encrpyt/decrypt
+    '''
+    # For plaintext key, use this
+    key = [ord(c) for c in key]
+    # If key is in hex:
+    # key = codecs.decode(key, 'hex_codec')
+    # key = [c for c in key]
+    keystream = PRGA(KSA(key))
+    # logger.debug(type(keystream))
+    
+
+    res = []
+    for c in text:
+        val = ("%02X" % (c ^ next(keystream)))  # XOR and taking hex
+        res.append(val)
+    return ''.join(res)
+
+
+def encrypt(key, plaintext):
+    ''' :key -> encryption key used for encrypting, as hex string
+        :plaintext -> plaintext string to encrpyt
+    '''
+    plaintext = [ord(c) for c in plaintext]
+    return encrypt_logic(key, plaintext)
+
+
+def decrypt(key, ciphertext):
+    ''' :key -> encryption key used for encrypting, as hex string
+        :ciphertext -> hex encoded ciphered text using RC4
+    '''
+    ciphertext = codecs.decode(ciphertext, 'hex_codec')
+    res = encrypt_logic(key, ciphertext)
+    return codecs.decode(res, 'hex_codec').decode('utf-8')
+
+
+def main():
+    key = 'not-so-random-key'  # plaintext
+    plaintext = 'Good work! Your implementation is correct'  # plaintext
+    # encrypt the plaintext, using key and RC4 algorithm
+    ciphertext = encrypt(key, plaintext)
+    print('plaintext:', plaintext)
+    print('ciphertext:', ciphertext)
+    # ..
+    # Let's check the implementation
+    # ..
+    ciphertext = '2D7FEE79FFCE80B7DDB7BDA5A7F878CE298615'\
+        '476F86F3B890FD4746BE2D8F741395F884B4A35CE979'
+    # change ciphertext to string again
+    decrypted = decrypt(key, ciphertext)
+    print('decrypted:', decrypted)
+
+    if plaintext == decrypted:
+        print('\nCongrats ! You made it.')
+    else:
+        print('Shit! You pooped your pants ! .-.')
+
+    # until next time folks !
+
+
+def test():
+    # Test case 1
+    # key = '4B6579' # 'Key' in hex
+    # key = 'Key'
+    # plaintext = 'Plaintext'
+    # ciphertext = 'BBF316E8D940AF0AD3'
+    assert(encrypt('Key', 'Plaintext')) == 'BBF316E8D940AF0AD3'
+    assert(decrypt('Key', 'BBF316E8D940AF0AD3')) == 'Plaintext'
+
+    # Test case 2
+    # key = 'Wiki' # '57696b69'in hex
+    # plaintext = 'pedia'
+    # ciphertext should be 1021BF0420
+    assert(encrypt('Wiki', 'pedia')) == '1021BF0420'
+    assert(decrypt('Wiki', '1021BF0420')) == 'pedia'
+
+    # Test case 3
+    # key = 'Secret' # '536563726574' in hex
+    # plaintext = 'Attack at dawn'
+    # ciphertext should be 45A01F645FC35B383552544B9BF5
+    assert(encrypt('Secret',
+                   'Attack at dawn')) == '45A01F645FC35B383552544B9BF5'
+    assert(decrypt('Secret',
+                   '45A01F645FC35B383552544B9BF5')) == 'Attack at dawn'
+
+if __name__ == '__main__':
+    main()
